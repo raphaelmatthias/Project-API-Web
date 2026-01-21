947daea9ab15fc633bc7bfafdd0deb610bec9db3
Raphael Mathias <rmra2202@gmail.com>
2026-01-21 14:35:46 -0300

    Adding SpecPattern for correct order of execution for Web and API

--- API Automation/cypress.config.js
+++ API Automation/cypress.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   e2e: {
+    specPattern: [
+      "cypress/support/Methods/RegisterMethods.js",
+      "cypress/support/Methods/CreateCartMethods.js"
+    ],
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    env: {
-    },
   },
-};
+}
--- Web Automation/cypress.config.js
+++ Web Automation/cypress.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   e2e: {
+    specPattern: [
+      "cypress/support/Pages/RegisterPage.js",
+      "cypress/support/Pages/LoginPage.js",
+      "cypress/support/Pages/CatalogPage.js"
+    ],
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-};
+}
